05c31217b7eb1a549d04bf7f694056323c0c9b80
jhuang-nowcom <55076345+jhuang-nowcom@users.noreply.github.com>
2019-09-09 13:07:28 -0700

    Update main.tf

--- server/main.tf
+++ server/main.tf
@@ -64,5 +64,10 @@ resource "aws_instance" "web" {
       "sudo sh /tmp/assets/setup-web.sh",
     ]
   }
+  
+  provider "aws" {
+  # MODIFY this line to look for 2.27.0 or greater
+  version = ">= 2.27.0"
+  }
 }
 
